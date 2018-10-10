841763c2ae09ad0a7701a6da860ddfaaf5388e1d
houlinan <houlinan@vip.qq.com>
2018-10-10 16:54:13 +0800

    11

--- src/main/java/cn/hgxsp/leileigongzuoshi/service/SearchService.java
+++ src/main/java/cn/hgxsp/leileigongzuoshi/service/SearchService.java
@@ -1,5 +1,6 @@
 package cn.hgxsp.leileigongzuoshi.service;
 
+import cn.hgxsp.leileigongzuoshi.DAO.BillDao;
 import cn.hgxsp.leileigongzuoshi.entity.Team;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
